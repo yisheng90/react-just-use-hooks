5fe954b51a64c6c3f2f4089ccf8a6bb2b701a0a6
Lee Yi Sheng <yisheng@yishenglee.com>
2020-03-26 18:48:40 +0800

    add test

---
+++ src/useComponentDidMount/useComponentDidMount.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { useComponentDidMount } from './useComponentDidMount';
+
+const TestComponent: React.FC = () => {
+  const [state, setState] = React.useState('not mount');
+  useComponentDidMount(() => setState('mounted'));
+
+  return <div>{state}</div>;
+};
+
+describe('useComponentDidMount', () => {
+  it('should evoke the callback after component is mounted', () => {
+    const { getByText } = render(<TestComponent />);
+
+    expect(getByText('mounted')).toBeTruthy();
+  });
+});
